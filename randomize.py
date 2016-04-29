dff96b8444d4dcb455e3091d569e117635facd9f
Michael Walker <michael@verus.io>
2016-04-29 18:18:45 -0400

    Added randomize email list.

---
+++ randomize.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+import sys
+import random
+
+if __name__ == '__main__':
+    with open(sys.argv[1], 'r') as f:
+        flist = f.readlines()
+        random.shuffle(flist)
+
+        for line in flist:
+            print line.strip()
+
