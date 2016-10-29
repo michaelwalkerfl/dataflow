d3df62c1bdc402f6f25cd651bfee1b4bed85675b
michaelew <michael@verus.io>
2016-10-29 12:18:47 -0400

    Updated dedupe.

--- dedupe/dedupe.py
+++ dedupe/dedupe.py
@@ -1,22 +1,20 @@
 #!/usr/bin/env python
 
 
-    def dedupe(args):
-        '''
-        Python is not a good solution for deduping larger files, awk is much faster.
-        awk '!x[$0]++' /path/to/file > /path/to/deduped_file
-        '''
-        from sys import argv
+def dedupe(args):
+    '''
+    Python is not a good solution for deduping larger files, awk is much faster.
+    awk '!x[$0]++' /path/to/file > /path/to/deduped_file
+    '''
+    email_list = args.list
+    with open(email_list, 'r') as f:
+        flist = f.readlines()
+        deduped = list(set(flist))
 
-        email_list = args.list
-        with open(email_list, 'r') as f:
-            flist = f.readlines()
-            deduped = list(set(flist))
-
-            with open('deduped_file.txt', 'w') as result:
-                for line in deduped:
-                    result.write(line)
-        return dedupe
+        with open('deduped_file.txt', 'w') as result:
+            for line in deduped:
+                result.write(line)
+    return dedupe
 
 
 if __name__ == '__main__':
--- manage.py
+++ manage.py
@@ -3,6 +3,7 @@
 def main():
     import argparse
     from database.send_to_database import send_to_db
+    from dedupe.dedupe import dedupe
 
     parser = argparse.ArgumentParser()
 
@@ -15,3 +16,8 @@ def main():
     send_to_db_parser.add_argument('--port', help='database port')
     send_to_db_parser.add_argument('--password', help='database password')
     send_to_db_parser.add_argument('--database', help='database name')
+
+    dedupe_parser = subparsers.add_parser('dedupe', help='dedupe list')
+    dedupe_parser.add_argument('list', help='list to dedupe')
+
+
