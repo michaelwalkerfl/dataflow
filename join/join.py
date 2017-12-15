4d754ee7f8a7eb19c4892def1282f6636c922299
Michael Walker <michaelwalker@beast.attlocal.net>
2017-12-14 23:46:45 -0500

    Fixed join function.

--- join/join.py
+++ join/join.py
@@ -2,19 +2,29 @@
 
 
 def join(args):
-    import glob
+    import os
 
     directory = args.dir
-    file_type = args.type
-    # Change to *.txt if needed.
-    files = glob.glob('{0}/*.{1}').format(directory, file_type)
+    dircontent = os.listdir(directory)
+    with open('joined.csv', 'w') as result:
+        for filename in dircontent:
+            file1 = open(directory + filename)
+            for line in file1:
+                result.write(line)
 
-    with open( 'joined.csv', 'w' ) as result:
-        for file_ in files:
-            for line in open( file_, 'r' ):
-                result.write( line )
-
+    file1.close()
+    result.close()
     return join
+    #file_type = args.type
+    # Change to *.txt if needed.
+    #files = glob.glob('{0}/*.{1}').format(directory, file_type)
+    #files = glob.glob('{0}/*.csv').format(directory)
+    #with open( 'joined.csv', 'w' ) as result:
+    #    for file_ in files:
+    #        for line in open( file_, 'r' ):
+    #            result.write( line )
+
+    #return join
 
 
 if __name__ == '__main__':
